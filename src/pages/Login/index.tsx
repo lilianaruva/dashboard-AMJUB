c474a5778ccc9ea3d7194d941fdbc99a4c48a187
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2025-03-02 23:24:59 -0600

    create scss base

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss'
+
+const Login = () => {
+    return (
+        <div className={styles.loginPage}>
+            <div className={styles.contentLogin}>
+                Login
+            </div>
+        </div>
+    )
+}
+
+export default Login
