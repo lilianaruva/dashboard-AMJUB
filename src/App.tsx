16bde6ac02df7688a57b14d27e06938b7a3b3892
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2025-03-02 23:36:28 -0600

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
-      <Login></Login>
+      <Dashboard/>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './components/layout/index.tsx'
+import Dashboard from './pages/Dashboard/index.tsx'
+import Login from './pages/Login/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />} >
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
